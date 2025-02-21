3cb37e02e72ec9cf3a0eab8ece5a45892a9e1b7b
Daniel Asaboro <asaborodaniel@gmail.com>
2025-02-21 16:24:49 +0100

    fixed name issue in dashboard

---
+++ src/app/(dashboard)/client-layout.tsx
@@ -0,0 +1,38 @@
+// /app/(dashboard)/client-layout.tsx
+"use client";
+
+import Sidebar from "@/components/navigation/sidebar";
+import Header from "@/components/navigation/header";
+import {
+  UserAccountProvider,
+  useUserAccount,
+} from "@/components/editor/context/account";
+
+function ApplicationContainer({ children }: { children: React.ReactNode }) {
+  const userAccount = useUserAccount();
+
+  return (
+    <>
+      <nav className="sticky top-0 max-h-screen">
+        <Sidebar />
+      </nav>
+      <main className="flex-1 bg-background">
+        <Header
+          userName={userAccount.name}
+          className="sticky top-0 z-10 w-full bg-card border-b border-border bg-background"
+        />
+        <div className="p-6">{children}</div>
+      </main>
+    </>
+  );
+}
+
+export function ClientLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-background">
+      <UserAccountProvider>
+        <ApplicationContainer>{children}</ApplicationContainer>
+      </UserAccountProvider>
+    </div>
+  );
+}
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,6 @@
 // /app/(dashboard)/layout.tsx
 import type { Metadata } from "next";
-import Sidebar from "@/components/navigation/sidebar";
-import Header from "@/components/navigation/header";
-import { UserAccountProvider } from "@/components/editor/context/account";
-
-
+import { ClientLayout } from "./client-layout";
 
 export const metadata: Metadata = {
   title: "Harmonia Dashboard",
@@ -13,19 +9,8 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-background">
-      <UserAccountProvider>
-        <nav className="sticky top-0 max-h-screen">
-          <Sidebar />
-        </nav>
-        <main className="flex-1 bg-background">
-          <Header
-            userName="Daniel Asaboro"
-            className="sticky top-0 z-10 w-full bg-card border-b border-border bg-background"
-          />
-          <div className="p-6">{children}</div>
-        </main>
-      </UserAccountProvider>
-    </div>
+    <ClientLayout>
+      {children}
+    </ClientLayout>
   );
 }
